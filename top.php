a6663b4da1feb645dc69e642ed409291dcf78953
evanray100 <34040612+evanray100@users.noreply.github.com>
2017-11-29 14:39:07 -0500

    Update top.php

--- top.php
+++ top.php
@@ -3,18 +3,18 @@ $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
 
 $path_parts = pathinfo($phpSelf);
 
- ?>	
+  ?>	
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Mark me wrong</title>
+        <title>Burlington Art</title>
 
         <meta charset="utf-8">
-        <meta name="author" content="Mark me wrong">
-        <meta name="description" content="Mark me wrong for not changing this">
+        <meta name="author" content="Evan M. Ray, Natasha Geffen, Hannah Meharg">
+        <meta name="description" content="Explore Burlingtons vast range of Art.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        
+        <link rel="stylesheet" href="../css/custom.css" type="text/css" media="screen">
 
         <?php
         $debug = false;
@@ -30,11 +30,11 @@ $path_parts = pathinfo($phpSelf);
 // PATH SETUP
 //
 
+$domain = '//';
 
+$server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, 'UTF-8');
 
-
-
-
+$domain .= $server;
 
 
         if ($debug) {
@@ -49,21 +49,21 @@ $path_parts = pathinfo($phpSelf);
 //
 // inlcude all libraries. 
 // 
-// 
-// 
+// Common mistake: not have the lib folder with these files.
+// Google the difference between require and include
 //
         print  PHP_EOL . '<!-- include libraries -->' . PHP_EOL;
 
+        require_once('lib/security.php');
 
-
-
-
-
-
-
-
-
-
+        // notice this if statemtent only includes the functions if it is 
+        // form page. A common mistake is to make a form and call the page
+        // join.php which means you need to change it below (or delete the if)
+        if ($path_parts['filename'] == "form") {
+            print PHP_EOL . '<!-- include form libraries -->' . PHP_EOL;
+            include 'lib/validation-functions.php';
+            include 'lib/mail-message.php';
+        }
 
         print  PHP_EOL . '<!-- finished including libraries -->' . PHP_EOL;
         ?>	
