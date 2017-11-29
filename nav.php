b58e144deb328faf2e8f490ca9c253010416c981
natashageffen <34040608+natashageffen@users.noreply.github.com>
2017-11-29 14:51:58 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,11 +2,51 @@
     <ol>
         <?php
         print '<li class="';
-        if ($path_parts['filename'] == "Art") {
+        if ($path_parts['filename'] == "Home") {
             print ' activePage ';
         }
         print '">';
-        print '<a href="art.php">Art</a>';
+        print '<a href="home.php">Home</a>';
+        print '</li>';
+        
+          print '<li class="';
+        if ($path_parts['filename'] == "About us") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="aboutus.php">About us</a>';
+        print '</li>';
+        
+          print '<li class="';
+        if ($path_parts['filename'] == "Contact") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="contact.php">Contact</a>';
+        print '</li>';
+        
+          print '<li class="';
+        if ($path_parts['filename'] == "Local Artists") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="artists.php">Local Artists</a>';
+        print '</li>';
+        
+          print '<li class="';
+        if ($path_parts['filename'] == "Local Galleries") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="galleries.php">Local Galleries</a>';
+        print '</li>';
+        
+          print '<li class="';
+        if ($path_parts['filename'] == "Submit Art") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="submit.php">Submit Art</a>';
         print '</li>';
           ?>
     </ol>
