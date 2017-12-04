0a07e045f76f70d0026163f3006bd6c86dcd1940
natashageffen <34040608+natashageffen@users.noreply.github.com>
2017-12-04 13:44:49 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,2 +1,26 @@
-
-<?php include "top.php" ?>
+ <?php
+include ('top.php');
+?>
+<article id="main">
+         <figure class="roundedCornersSmall fiftyPercent animated">       
+                <img alt="" class="roundedCornersSmall" src="../images/.gif">
+            <figcaption></figcaption>
+  </figure>
+  
+  <h1> About Us </h1>
+  
+  <h2>Hannah Meharg</h2>
+  <p></p>
+  
+  <h2>Evan Ray</h2>
+  <p></p>
+  
+  <h2>Natasha Geffen</h2>
+  <p></p>
+  
+</aside>
+<?php
+include "footer.php";
+?>
+    </body>
+</html>
