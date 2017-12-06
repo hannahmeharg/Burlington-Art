057de6e7bc2123ee0ea2ecf255c946b395a043cb
hannahmeharg <34040539+hannahmeharg@users.noreply.github.com>
2017-12-06 17:49:09 -0500

    Create art.php

---
+++ art.php
@@ -0,0 +1,58 @@
+<?php
+include ('top.php');
+?>
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <head>
+            <link rel="stylesheet" href="artcss.css">
+            <link rel="icon" href="../images/beloved.jpg" type="image/x-icon" />
+            <!--<link rel="icon" href="https://4221b26a72c93d47ccd2-604a02ecb66114c294e75afff36cd045.ssl.cf1.rackcdn.com/favicon_Ic1_icon.ico" type="image/x-icon" />
+            -->
+        
+        </head>         
+        <div class="row">
+            <div class="column">
+                <img src="../images/algae-3.jpg" style="width: 100% " />
+                <img src="../images/ed-owre_.jpg" style="width: 100%" />
+                <img src="../images/behind-orion.jpg" style="width: 100%" />
+                <img src="../images/new-york-city-starbucks.jpg" style="width: 100%"/>
+                <img src="../images/taken.jpg" style="width: 100%"/>
+                <img src="../images/tower.jpg" style="width: 100%"/>
+                <img src="../images/money-right-5.jpg" style="width: 100%"/>
+                <img src="../images/beloved.jpg" style="width: 100%"/>
+            </div>
+            <div class="column">
+                <img src="../images/curiosity.jpg" style="width: 100%"/>
+                <img src="../images/serpent.jpg" style="width: 100%"/>
+                <img src="../images/marionnette.jpg" style="width: 100%"/>
+                <img src="../images/watching-the-sun-rise.jpg" style="width: 100%"/>
+                <img src="../images/tom.jpg" style="width: 100%"/>
+                <img src="../images/toast.jpg" style="width: 100%"/>
+                <img src="../images/amanda1.jpg" style="width: 100%"/>
+                <img src="../images/elise-header.jpg" style="width: 100%"/>
+                <img src="../images/contemplation.jpg" style="width: 100%"/>
+            </div>
+            <div class="column">
+                <img src="../images/dustyheader.jpg" style="width: 100%"/>
+                <img src="../images/man-at-gugg.jpg" style="width: 100%"/>
+                <img src="../images/root-system.jpg" style="width: 100%"/>
+                <img src="../images/disaster.jpg" style="width: 100%"/>
+                <img src="../images/final-outpost-1.jpg" style="width: 100%"/>
+                <img src="../images/pete2.jpg" style="width: 100%"/>
+                <img src="../images/neil.jpg" style="width: 100%"/>
+                <img src="../images/Bushey.jpg" style="width: 100%"/>
+                <img src="../images/collapse.jpg" style="width: 100%"/>
+       </div>
+            
+       
+    </div>
+             
+     
+    </body>
+    <br> 
+</html>
+
+<?php
+include ('footer.php');
+?>
