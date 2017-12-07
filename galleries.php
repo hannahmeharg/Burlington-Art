4a6b749457bf423560884b3b9de88b2037280d9d
evanray100 <34040612+evanray100@users.noreply.github.com>
2017-12-07 03:53:24 -0500

    Update galleries.php

--- galleries.php
+++ galleries.php
@@ -1,1 +1,32 @@
+<?php
+include ('top.php');
+?>
 
+<h1>Burlington&apos;s Local Galleries/Museums</h1>  
+<h2 class="fleming">Robert Hull Fleming Museum</h2>   
+<p><img src="../images/fleming.jpg" alt="" class="photoFleming">
+The University of Vermont is a proud owner of the Robert Hull Fleming Museum, a cultural treasure prized by Vermonters, UVM students, UVM faculty and visitors for more than 80 years.  The Fleming Museum is location on UVM’s campus, which, founded in 1791, is one of the country's oldest universities.  Fleming is a beautiful museum with incredible exhibits displaying art pieces and artifacts from all over the world.  Since opening, it has been hailed as "a practical place of learning — a vibrant, ongoing educational institution for both children and adults."  The Fleming museum has a long-term collection of art pieces and artifacts from Africa, America, Asia and Europe, as well as galleries with the theme of Native America culture, Oceanic and pre-Columbia.  With over 20,000 objects spanning the history of human civilization, the Fleming Museum offers an irreplaceable opportunity to study art work, and artifacts from all over the world.  In fact, Evan Ray and Hannah Meharg get to enjoy this spectacular display of arts multiple times a week as proud employees of the Fleming Museum!</p>
+
+<h2 class="bca">The Burlington City Arts Center</h2>
+<p><img src="../images/bca.jpg" alt="" class="photoBCA">
+The Burlington City Arts Center Exhibits nationally ranked artists stemming from Vermont to all across the country.  The BCA Center has a mission is to bring a unique art experience to the public’s eye.  The ultimate goal of doing so is to challenge, teach and engage the visitors. The BCA Center contains three floors displaying a wide range of contemporary art exhibitions.  The BCA Center offers frequent music, film and performance events to the public throughout the year.  The BCA center has helped satisfy the cultural and artistic needs of Burlington for more than 30 years.  Their vision is to close the gap between arts and the community, fusing the two in order to create a dynamic relationship nurturing creativity, ingenuity, imagination and sparking interest in those who might not be familiar with the unparalleled glory the art world offers.  The BCA Center is one of the few exhibition spaces in Vermont that only displays modern art. </p>
+
+<h2 class="frog">Frog Hollow Vermont State Craft Center</h2>
+<p><img src="../images/frog.jpg" alt="" class="photoFrog">
+The Frog Hollow Vermont State Craft Center is a 501C3 non-profit organization founded after its parent establishment, the Frog Hollow Craft Association.  The Frog Hollow Vermont State Craft Center is committed to the exposure and appreciation of Vermont’s high quality craftsmanship.  The Frog Hollow’s gallery is also located on Church street in Burlington, Vermont.  The gallery exhibits a unique and exceptional collection of tradition and contemporary Vermont art and craft.  Over the past 40 years the Frog Hollow’s Vermont State Craft Center has worked with community’s state wide to support, develop and educate children, young artists and community members.  Their education programs teach these individuals about the world of arts and crafts.  Frog Hollow’s received the distinction of the first state craft center in the nation in 1975.  Despite this distinction, the organization has no state funding and only runs on gallery sales, their artisan community, and private donations.  If you come and enjoy the craft center, please consider making a donation to keep this wonderful organization thriving!</p>
+
+<h2 class="champlain">Champlain College Art Gallery</h2>
+<p><img src="../images/champlain.jpg" alt="" class="photoChamplain">
+The Champlain College Art Gallery location on Maple street in Burlington, Vermont is a supporter of innovative and experimental modern art.  They have a versatile collection of contemporary art in all mediums.  The gallery exhibits a range of artists from Champlain college students and regional artists to artists from all across the country.  Through a “rapid-fire” program of events, exhibitions, residencies and projects, the gallery strives for a hybrid of art and technology.  The gallery acts as a catalyst for propelling collaboration and learning for Champlain students.  The Champlain College Arts Gallery is open to the public and free.  Please stop by and make sure to check our Events page to stay informed about new events at the Champlain College Arts gallery!</p>
+
+<h3>Here's a map of all the local Galleries and Museums in the Burlington area!</h3>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d16965.431875819748!2d-73.2144748273686!3d44.470796105894635!3m2!1i1024!2i768!4f13.1!2m1!1slocal+galleries+near+me!5e0!3m2!1sen!2sus!4v1512626973664" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
+
+<?php
+include ('footer.php');
+?>
+</body>
+</html>
