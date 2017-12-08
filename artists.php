38612fe3e4d0932911a88fb173c0359e16b3ad69
natashageffen <34040608+natashageffen@users.noreply.github.com>
2017-12-07 19:39:18 -0500

    Update artists.php

--- artists.php
+++ artists.php
@@ -1,1 +1,59 @@
 
+<?php
+include ('top.php');
+?>
+
+<article id="artists">
+    
+    <title>Local Artists</title>
+    
+    
+    <h2>Robert Gold</h2>
+    <figure>
+        <img src="../final/Gold.jpg" alt="" class="artist1">
+              <figcaption>Robert Gold has been a lifelong artist at heart, even when he was teaching dentistry at Harvard or being a captain in the army. After having a traumatic brain injury, however, he devoted his life to art. His art has been exhibited in various Burlington locations, from the UVM Medical Center to the Burlington International Airport. He also won the People’s Choice Award at the South End Art Hop in Burlington in 2016.
+<br>
+<br>
+Gold enjoys combining photography and acrylic paint in his work. While focusing on the camera’s composition, he uses digital manipulation and paint to make bring vibrancy to his prints. He strives to tell stories with his artwork.
+</figcaption>
+            </figure>
+    
+    <h2>Jennie Kristel</h2>
+     <figure>
+         <img src="../final/Kristel.jpg" alt="" class="artists">
+              <figcaption>Jennie Kristel studied art in Massachusetts when she was just in high school and went on to get her MA in Expressive Therapies at Lesley University. She now lives in Burlington, where is an expressive therapist. She uses art to work as a teacher and healer. Her art has been featured at Vermont Studio Center as well as exhibits throughout the Northeast.
+<br>
+<br>
+Kristel creates 2D art, such as paintings and prints, along with functional objects, such as lampshades, journals, and handmade envelopes. She also uses art to express the connection between human experience and the natural landscape. Her work focuses on spirituality and emotion and uses objects such as feathers and leaves. 
+</figcaption>
+            </figure>
+    
+    <h2>Winnie Looby</h2>
+     <figure>
+         <img src="../final/Looby.jpg" alt="" class="artists">
+              <figcaption>Winnie Looby is a multi-disciplinary artist who, along with visual arts, has experience in the performing arts as a musician, dancer, and actress. Visual art, however, is what she first became passionate about. Both her solo and collaborative projects have been featured in shows at the Rose Street Artists’ Cooperative, ONE Arts Center, Burlington City Arts, and FLYNNDOG. She is currently in the Education PhD program at UVM.
+<br>
+<br>
+Looby’s work explores digital art and costume design for modern dance performances. Her art is inspired by surrealism and mythology as well as experiences in everyday life. 
+</figcaption>
+            </figure> 
+    
+    <h2>Elise Whittemore</h2>
+     <figure>
+         <img src="../final/Whittemore.jpg" alt="" class="artist2">
+              <figcaption>Elise Whittemore has a BFA in printmaking, photography, and design from Syracuse University. She is a painter, printmaker, and editorial designer for books and magazines. Her work has been included in the Brooklyn Waterfront Artists’ Coalition in New York and 215 College Gallery in Burlington. 
+<br>
+<br>
+
+Whittemore specializes in woodcut printing and lithography. Her work focuses on plant life and other nature-related contexts. This collection of algae pieces are made of woodcut monoprint and uses layers of cut paper pieces, wood, and ink.
+</figcaption>
+            </figure>
+    
+</article>
+
+
+        <?php
+        include('footer.php');
+        ?>
+</body>
+</html>
