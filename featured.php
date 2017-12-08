bf630b501ceb097e9e762bfd32a1352cbc70e343
hannahmeharg <34040539+hannahmeharg@users.noreply.github.com>
2017-12-07 19:00:14 -0500

    Update featured.php

--- featured.php
+++ featured.php
@@ -1,1 +1,192 @@
 
+<?php
+include ('top.php');
+?>
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <head>
+            <link rel="stylesheet" href="artcss.css">
+            <link rel="icon" href="../images/beloved.jpg" type="image/x-icon" />
+            <!--<link rel="icon" href="https://4221b26a72c93d47ccd2-604a02ecb66114c294e75afff36cd045.ssl.cf1.rackcdn.com/favicon_Ic1_icon.ico" type="image/x-icon" />
+            -->
+        
+        </head>         
+        <div class="row">
+            <div class="column">
+                <div class="container">
+                    <img class="image" src="../images/algae-3.jpg" style="width: 100% " />
+                    <div class="overlay">
+                        <div class="text">'Algae 3'<br>Elise Whittemore</div>
+                      
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/ed-owre_.jpg" style="width: 100%" />
+                    <div class="overlay">
+                        <div class="text">'Event Horizon'<br>Ed Owre</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/behind-orion.jpg" style="width: 100%" />
+                    <div class="overlay">
+                        <div class="text">'Behind Orion'<br> David Russel</div>
+                       
+                    </div>
+                </div>
+                 <div class="container">
+                    <img class="image" src="../images/new-york-city-starbucks.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'New York City Starbucks'<br>Robert Gold</div>
+                    </div>
+                </div>
+               <div class="container">
+                    <img class="image" src="../images/taken.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Taken'<br>Jean Cherouny</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/tower.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Tower'<br>Frank DeAngelis</div>
+                    </div>
+                </div>
+                 <div class="container">
+                    <img class="image" src="../images/money-right-5.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">Money right</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/beloved.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">    'Beloved'<br>Linda Di Sante</div>
+                    </div>
+                </div>
+               <div class="container">
+                    <img class="image" src="../images/contemplation.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Contemplation, Lake Champlain'<br>Kurt Madison</div>
+                    </div>
+                </div> 
+            </div>
+           
+            <div class="column">
+                <div class="container">
+                    <img class="image" src="../images/curiosity.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">   'Curiosity'<br>David Russel</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/serpent.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">   'Serpent'<br>Jean Cherouny</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/marionnette.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Marionnette'<br>Winnie Looby</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/watching-the-sun-rise.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Watching the Sun Rise'<br>Jennie Kristel</div>
+                    </div>
+                </div>
+                 <div class="container">
+                     <img class="image" src="../images/the-hill.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">     'The Hill'<br>Frank DeAngelis</div>
+                    </div>
+                </div>
+                  <div class="container">
+                    <img class="image" src="../images/toast.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">TOAST</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/amanda1.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">AMANDA</div>
+                    </div>
+                </div>
+                 <div class="container">
+                     <img class="image" src="../images/elise_whittemore_specimen_book.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">ELISE</div>
+                    </div>
+                </div> 
+                
+            </div>
+            
+            <div class="column">
+                <div class="container">
+                   <img class="image" src="../images/humblepie.jpg" style="width: 100%"/>
+                   <div class="overlay">
+                       <div class="text">'Humble Pie'<br>Dust Boyton</div>
+                   </div>
+                   
+                </div>
+                
+                <div class="container">
+                    <img class="image" src="../images/man-at-gugg.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Man at the Guggenheim Museum'<br>Robert Gold</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/root-system.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Root System'<br>Winnie Looby</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/disaster.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">   'Disaster'<br>Frank DeAngelis</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/final-outpost-1.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">'Final Outpost, Burlington, Vermont'<br>Jennie Kristel</div>
+                    </div>
+                </div>
+                <div class="container">
+                    <img class="image" src="../images/pete2.jpg" style="width: 100%"/>
+                    <div class="overlay">
+                        <div class="text">PETE</div>
+                    </div>
+                </div>
+                <div class="container">
+                   <img class="image" src="../images/Bushey.jpg" style="width: 100%"/>
+                   <div class="overlay">
+                       <div class="text">BUSH</div>
+                   </div>
+                </div>
+                <div class="container">
+                   <img class="image" src="../images/collapse.jpg" style="width: 100%"/>
+                   <div class="overlay">
+                       <div class="text">Beloved</div>
+                   </div>
+                   
+                </div>
+               
+       </div>
+            
+       
+    </div>
+             
+     
+    </body>
+    <br> 
+</html>
+
+<?php
+include ('footer.php');
+?>
